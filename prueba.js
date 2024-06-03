d586d5b060bd16208f90849a2f9fce945154f237
Jordán Osorio B. <jordanjesusosoriobustos@gmail.com>
2024-06-02 22:06:16 -0400

    firstcommit

--- prueba.js
+++ prueba.js
@@ -2,55 +2,71 @@
 utulizar para la función ver más*/
 
 function verMas(NombreCategoria) {
-  // Realiza una nueva llamada AJAX para obtener los platos de la categoría seleccionada
-  $.get(
-    "https://www.themealdb.com/api/json/v1/1/filter.php?c=NombreCategoria",
-    function (data) {
-      // Limpia la tabla anterior
-      $("#tabla").empty();
+  var categoriaId = `detalle-${NombreCategoria}`;
+  //Aqui es solo para habilitar o deshabilitar ls visibilidad de la sigueinte fila
+  $(`#${categoriaId}`).toggle();
+  $.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${NombreCategoria}`, function (data) {
 
-      // Construye la nueva tabla con los datos obtenidos
-      $.each(data.NombreCategoria, function (i, meal) {
-        let htmlFila = "<tr><td>" + meal.meals + "</td></tr>";
-        $("#tabla-detalle").append(htmlFila);
+      //Aqui se eliminará los elementos de este item que antes se hayan cargado evitando que se comiencen a repetir
+      $(`#tabla-detalle-${NombreCategoria}`).empty();
+      
+      $.each(data.meals, function (i, meal) {
+          let htmlFila = `
+              <tr>
+                  <td>${meal.idMeal}</td>
+                  <td>${meal.strMeal}</td>
+                  <td><img src="${meal.strMealThumb}" style="max-width: 100px;"></td>
+              </tr>`;
+          $(`#tabla-detalle-${NombreCategoria}`).append(htmlFila);
       });
-    }
-  );
+  });
 }
 
-$(document).ready(function () {
-  console.log("Hola mundo");
+'++'
 
-  // funcion que permite cargar el api de categorias de comidas
+$(document).ready(function () {
+
+  // Función que permite cargar el API de categorías de comidas
 
   /*mostrando las categorías de comida*/
   $("#btn-cargar").on("click", function () {
-    console.log("cargando informacion...");
-    $("#spinner").html("");
+      console.log("cargando informacion...");
+      $("#spinner").html("");
 
-    let htmlSpinner = '<div class="lds-circle"><div></div></div>';
+      let htmlSpinner = '<div class="lds-circle"><div></div></div>';
+      $("#spinner").append(htmlSpinner);
 
-    $("#spinner").append(htmlSpinner);
+      $.get("https://www.themealdb.com/api/json/v1/1/categories.php", function (data) {
+        $(`#tabla`).empty();
 
-    $.get(
-      "https://www.themealdb.com/api/json/v1/1/categories.php",
-      function (data) {
-        $.each(data.categories, function (i, item) {
-          let htmlFila =
-            '<tr id="fila-1"><th scope="row">' +
-            item.idCategory +
-            "</th><td>" +
-            item.strCategory +
-            '</td><td><img src="' +
-            item.strCategoryThumb +
-            '" style="max-width: 150px;"></td><td><button onclick="verMas(1)"id="verMas" class="btn btn-dark"><i class="fa-solid fa-plus"></i></i> Ver más</button></td></tr>';
-
-          $("#tabla").append(htmlFila);
-        });
-        $("#spinner").html("");
-      }
-    );
+          $.each(data.categories, function (i, item) {
+            //propongo usar ${} en vez de '++' encuentro que es una practica mas ordenada y visiblemente mas clara
+              let htmlFila = `
+                  <tr id="fila-${item.idCategory}">
+                      <th scope="row">${item.idCategory}</th>
+                      <td>${item.strCategory}</td>
+                      <td><img src="${item.strCategoryThumb}" style="max-width: 150px;"></td>
+                      <td><button onclick="verMas('${item.strCategory}')" class="btn btn-dark"><i class="fa-solid fa-plus"></i> Ver más</button></td>
+                  </tr>
+                  <tr class="nested-table" id="detalle-${item.strCategory}" style="display:none;">
+                      <td colspan="4">
+                          <table class="table table-bordered">
+                              <thead>
+                                  <tr>
+                                      <th>ID </th>
+                                      <th>Platillo </th>
+                                      <th>Imagen </th>
+                                  </tr>
+                              </thead>
+                              <tbody id="tabla-detalle-${item.strCategory}"></tbody>
+                          </table>
+                      </td>
+                  </tr>`;
+              $("#tabla").append(htmlFila);
+          });
+          $("#spinner").html("");
+      });
   });
-  /*Aquí se agrega la función de verMAs */
-  /*Aquí se establece la función para ver más alimentos de la categoría seleccionada */
 });
+
+
